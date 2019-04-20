a1e238167c247a0670aa447d8e53f93c75b02252
Savithri <mnsjayathilaka86@gmail.com>
2019-04-20 19:44:07 +0530

    Initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,66 @@
+import {Component} from '@angular/core';
+
+@Component({
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+    selectedIndex = 0;
+    const
+    menuCategoryList = [
+        {
+            name: 'Content',
+            children: [
+                {
+                    name: 'Customer',
+                    icon: 'person',
+                    routerLink: '/customers'
+                },
+                {
+                    name: 'Order',
+                    icon: 'book',
+                    routerLink: '/orders'
+                },
+                {
+                    name: 'Message',
+                    icon: 'message',
+                    routerLink: '/messages'
+                }
+            ]
+        },
+        {
+            name: 'Report',
+            children: [
+                {
+                    name: 'Sales',
+                    icon: 'attach_money',
+                    routerLink: ''
+                },
+                {
+                    name: 'Products',
+                    icon: 'storage',
+                    routerLink: ''
+                }
+                , {
+                    name: 'Customers',
+                    icon: 'people',
+                    routerLink: ''
+                }
+            ]
+        }
+    ];
+
+
+    isPanelExpanded(index) {
+        if (this.selectedIndex === index) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    onMenuCategoryClick(selectedIndex) {
+        this.selectedIndex = selectedIndex;
+    }
+}
---
+++ src/app/customers/customers.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { CustomersRoutingModule } from './customers-routing.module';
+import { CustomerListComponent } from './customer-list/customer-list.component';
+import { CustomerCardComponent } from './customer-card/customer-card.component';
+
+import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
+import {MatCardModule} from '@angular/material/card';
+
+
+
+@NgModule({
+  declarations: [CustomerListComponent, CustomerCardComponent],
+  imports: [
+    CommonModule,
+    CustomersRoutingModule,
+
+    MatButtonModule,MatIconModule,MatCardModule
+  ]
+})
+export class CustomersModule { }
