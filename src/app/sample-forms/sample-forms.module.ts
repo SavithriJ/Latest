d3858b07f0700536a6f908cd972fd221a31f9d3a
Savithri <mnsjayathilaka86@gmail.com>
2019-04-21 03:22:28 +0530

    adding Forms module

---
+++ src/app/sample-forms/sample-forms.module.ts
@@ -0,0 +1,33 @@
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {SampleFormComponent} from './sample-form/sample-form.component';
+import {SampleFormsRoutingModule} from './sample-forms-routing.module';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {
+    MatButtonModule,
+    MatCardContent, MatCardModule, MatDatepickerModule,
+    MatFormFieldModule,
+    MatIconModule,
+    MatInputModule, MatNativeDateModule,
+    MatSelectModule
+} from '@angular/material';
+
+@NgModule({
+    declarations: [SampleFormComponent],
+    imports: [
+        CommonModule,
+        SampleFormsRoutingModule,
+        ReactiveFormsModule,
+        FormsModule,
+        MatFormFieldModule,
+        MatInputModule,
+        MatButtonModule,
+        MatIconModule,
+        MatSelectModule,
+        MatCardModule,
+        MatDatepickerModule,
+        MatNativeDateModule
+    ]
+})
+export class SampleFormsModule {
+}
