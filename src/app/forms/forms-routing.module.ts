363d830ef9adfd6aff643b1e08cf2f8bee7397af
Savithri <mnsjayathilaka86@gmail.com>
2019-04-21 00:13:25 +0530

    sample form added

---
+++ src/app/forms/forms-routing.module.ts
@@ -0,0 +1,17 @@
+import {NgModule} from '@angular/core';
+import {Routes, RouterModule} from '@angular/router';
+import {SampleFormComponent} from './sample-form/sample-form.component';
+
+const routes: Routes = [
+    {
+        path: '',
+        component: SampleFormComponent
+    }
+];
+
+@NgModule({
+    imports: [RouterModule.forChild(routes)],
+    exports: [RouterModule]
+})
+export class FormsRoutingModule {
+}
